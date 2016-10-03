1dc77385ce8ef877b0eda5052c6eef9e84b1146e
Christopher <channon@hawk.iit.edu>
2016-10-03 22:45:06 +0000

    removing trash

--- .vtgpio_test.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/emb-vt/vtgpio_test.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/pi/emb-vt/vtgpio_test.ko /home/pi/emb-vt/vtgpio_test.o /home/pi/emb-vt/vtgpio_test.mod.o
