14c7051f7a067b03d716065668248599f86107f7
neil <ngetty@hawk.iit.edu>
2017-04-20 16:09:40 -0500

    test for vt

---
+++ src/tests/B.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# this program recieves the message from program A and prints it to a file
+
+import signal,os
+import time
+import sys
+
+RUN_TIME = 60 # seconds
+if len(sys.argv) > 1:
+    print('hi')
+    RUN_TIME = int(sys.argv[1])
+
+
+def setup_pipe():
+    pipe_name = '/tmp/vtime_test.pipe'
+    if not os.path.exists(pipe_name):
+        os.mkfifo(pipe_name)
+    return open(pipe_name, 'r')
+
+def listen(pipe):
+    return pipe.readline()
+
+if __name__ == '__main__':
+    start_time = time.time()
+    my_pipe = setup_pipe()
+    #while time.time() < (start_time + RUN_TIME):
+    while 1:
+        A = listen(my_pipe)
+        if A != "exit":
+            print('Program A time: %sProgram B time: %s \n' % ( A , time.time()))
+        else:
+            sys.exit(0)
