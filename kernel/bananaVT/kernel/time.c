76a368ae738d959aea8d3dd50e2f1a211485f047
root <admin@hawk.iit.edu>
2017-04-20 20:42:46 +0000

    try kill_pid instead of kill_pgrp

--- kernel/bananaVT/kernel/time.c
+++ kernel/bananaVT/kernel/time.c
@@ -224,10 +224,10 @@ void freeze_time(struct task_struct *tsk)
   struct timespec ts;
   s64 now;
   int kill_status=0;
-  
-  printk("VT-DEBUG: in freeze_time");
+ 
+  //printk("VT-DEBUG: in freeze_time\n");
   /* signal STOP to freeze this @tsk's children */;
-  kill_status = kill_pgrp(task_pid(tsk), SIGSTOP, 1);
+  kill_status = kill_pid(task_pid(tsk), SIGSTOP, 1);
   getnstimeofday(&ts);
   now = timespec_to_ns(&ts);
   /**
@@ -235,7 +235,7 @@ void freeze_time(struct task_struct *tsk)
    * so we MUST NOT zero it here
    */
   tsk->freeze_start_nsec = now;
-  printk("VT-DEBUG: returning from freeze_time with %llun sec and kill status %d", now,kill_status);
+  printk("VT-DEBUG: returning from freeze_time with %llun sec and kill status %d \n", now,kill_status);
 }
 EXPORT_SYMBOL(freeze_time);
 
@@ -251,7 +251,7 @@ void unfreeze_time(struct task_struct *tsk)
   s64 now;
   int kill_status=0;
 
-  printk("VT-DEBUG: in unfreeze_time");
+  //printk("VT-DEBUG: in unfreeze_time\n");
   getnstimeofday(&ts);
   now = timespec_to_ns(&ts);
   tsk->freeze_past_nsec += (now - tsk->freeze_start_nsec);
@@ -260,8 +260,8 @@ void unfreeze_time(struct task_struct *tsk)
   populate_frozen_time(tsk);
 
   /* signal CONTINUE to unfreeze @tsk's children after timekeeping */
-  kill_status = kill_pgrp(task_pid(tsk), SIGCONT, 1);
-  printk("VT-DEBUG: returning from unfreeze_time with %llun nsec and kill status %d", now, kill_status);
+  kill_status = kill_pid(task_pid(tsk), SIGCONT, 1);
+  printk("VT-DEBUG: returning from unfreeze_time with %llun nsec and kill status %d\n", now, kill_status);
 
 }
 EXPORT_SYMBOL(unfreeze_time);
