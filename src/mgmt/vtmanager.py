88300b5943259a9137ea0b9b5ea85d15172c3759
Br1an6 <leaderrorme@gmail.com>
2018-02-07 11:20:51 -0600

    add classes for node type todo reutrn the function

--- src/mgmt/vtmanager.py
+++ src/mgmt/vtmanager.py
@@ -6,15 +6,13 @@ import zmq
 
 class HostService(object):
     """class of service handling"""
-    def __init__(self, type_of_service, node_id, node_value, proc_status):
+    def __init__(self, node_id, node_value, proc_status):
         """
         Args:
-        type_of_service...int 1. Controller 2. Sensor
         noe_id............int
         node_value........list of float
         proc_status.......dict {123:True, 124:False}  bool true unfreeze false freeze
         """
-        self.type_of_service = type_of_service
         self.node_id = node_id
         self.node_value = node_value
         self.proc_status = proc_status # no more than 16
@@ -35,6 +33,35 @@ class HostService(object):
         self.proc_status = proc_status
 
 
+class ControllerHandler(HostService):
+    """Class to handle behavior of Controller"""
+    def __init__(self, type_of_service, node_id, node_value, proc_status):
+        """
+        Args:
+        type_of_service...int 1. Controller 2. Sensor
+        noe_id............int
+        node_value........list of float
+        proc_status.......dict {123:True, 124:False}  bool true unfreeze false freeze
+        """
+        HostService.__init__(self, node_id, node_value, proc_status)
+        self.type_of_service = type_of_service
+
+
+
+class SensorHandler(HostService):
+    """Class to handle behavior of Sensor"""
+    def __init__(self, type_of_service, node_id, node_value, proc_status):
+        """
+        Args:
+        type_of_service...int 1. Controller 2. Sensor
+        noe_id............int
+        node_value........list of float
+        proc_status.......dict {123:True, 124:False}  bool true unfreeze false freeze
+        """
+        HostService.__init__(self, node_id, node_value, proc_status)
+        self.type_of_service = type_of_service
+
+
 def process_handler(opt, server_localhost):
     """
     Args:
@@ -52,7 +79,7 @@ def process_handler(opt, server_localhost):
         f1 = open(file_name, 'w')
         f1.write(str(key))
         f1.close()
-    
+
     server_localhost.setStatus(cp_proc_dict)
 
     if opt == 'STOP':
@@ -109,7 +136,7 @@ def start_listener(server_localhost):
         # target_ids_list = work['NodeIDs']
         # handling stop or resume process
         print '[*] Receive', opt, ' From :', from_sender, '  ', time.ctime()
-        process_handler(opt, server_localhost)
+        # process_handler(opt, server_localhost) # take out this part for testing
 
 
 def get_sensor_data():
@@ -123,26 +150,31 @@ def get_sensor_data():
 
 def value_retriever(server_localhost):
     """
-    start the service for retrieving data and handling pause 
+    start the service for retrieving data and handling pause
     Arg:
     server_localhost...HostService
     """
 
     while True:
         time.sleep(1)
+        """ # take out this part for loopback
+        while (False in server_localhost.getStatus().values()): # to lock the proc while in the stop state
+            time.sleep(1)
+            print '[*] Retrieving Pause'
+        """
         sensor_val = get_sensor_data()
-        print '[*] Value: ', sensor_val 
+        print '[*] Value: ', sensor_val
         if sensor_val < 2: # assume this is the situation we need to pause the system
             print '[*] Not getting value, system pasuing'
-            send_command('STOP', server_localhost)           
+            send_command('STOP', server_localhost)
 
-            # wait until you get the value
+            #  TODO: Shoudl have a lock here, wait until you get the value 
             while sensor_val < 2:
                 time.sleep(1)
                 sensor_val = get_sensor_data()
-                print '[*] Requesting updated Value: ', sensor_val 
-            send_command('RESUME', server_localhost)   
-              
+                print '[*] Requesting updated Value: ', sensor_val
+            send_command('RESUME', server_localhost)
+
         server_localhost.setValue([sensor_val])
 
 
@@ -163,10 +195,17 @@ def main():
     # for now both of them do the same thing
     if type_of_service == 1 or type_of_service == 2:
         # add real value
-        server_localhost = HostService(type_of_service, node_id, [0.], {0:True}) # init
+        if type_of_service == 1:
+            server_localhost = ControllerHandler(type_of_service, node_id, [0.], {0:True}) # init
+            print "[*] Starting Controller"
+        elif type_of_service == 2:
+            server_localhost = SensorHandler(type_of_service, node_id, [0.], {0:True}) # init
+            print "[*] Starting Sensor"
+
         print "[*] Starting Service ID: #%s" % (server_localhost.getID())
         multiprocessing.Process(name='p1', target=start_listener, args=(server_localhost,)).start()
         print "[*] Listening..."
+        # TODO: depends on the service type
         multiprocessing.Process(name='p2', target=value_retriever,  args=(server_localhost,)).start()
         print "[*] Retrieving..."
     else:
