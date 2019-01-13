7b99a1acbb002904945fdde5d08b71dff4c056de
root <admin@hawk.iit.edu>
2019-01-13 16:02:50 -0600

    update for LAN IPs

--- eval/networking/thru/server.sh
+++ eval/networking/thru/server.sh
@@ -9,7 +9,7 @@ make
 insmod vtgpio.ko
 cd /home/emb-vt/eval/networking/thru/
 # clear dmesg
-sleep 5
+sleep 1
 dmesg --clear
 ########################
 ### setup experiment ###
@@ -17,13 +17,17 @@ dmesg --clear
 
 sleep 1
 
-./start_paused.sh iperf -u -s & #c 192.168.0.2 -b 100M -t 32 -i 1 -y c -x MV > iperf_${1}.log &
+#./start_paused.sh iperf3 -u -s & #c 192.168.0.2 -b 100M -t 32 -i 1 -y c -x MV > iperf_${1}.log &
+# no -u
+./start_paused.sh iperf3 -s & 
 
-echo $! > /sys/vt/VT7/pid_01
+PID1=$!
 
-sleep 2
+#echo $PID1 > /sys/vt/VT7/pid_01
 
-kill -CONT $!
+sleep 1
+
+kill -CONT $PID1
 
 wait $PID1
 
